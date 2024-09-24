63a7fbef2affeb5766e9eb5f3cf2090615733607
Eric Yancey Dauenhauer <eric@ericyd.com>
2024-09-24 20:44:24 +0000

    use server.boundary

--- test/TestComponent.spec.ts
+++ test/TestComponent.spec.ts
@@ -1,19 +1,20 @@
 import { describe, expect, it } from "vitest";
 import { flushPromises, mount } from '@vue/test-utils'
 import TestComponent from '../src/TestComponent.vue'
+import { server } from './msw'
 
 describe('TestComponent', () => {
-  it('test 1', async () => {
+  it('test 1', server.boundary(async () => {
     const wrapper = mount(TestComponent)
 
     await flushPromises()
 
     expect(wrapper.find('pre').exists()).toBe(true)
-  })
+  }))
 
-  it('test 2', async () => {
+  it('test 2', server.boundary(async () => {
     const wrapper = mount(TestComponent)
 
     expect(wrapper.find('pre').exists()).toBe(true)
-  })
+  }))
 })
